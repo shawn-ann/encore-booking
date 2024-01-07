07204649d00646ff307281aadb81d93de9584372
FinnsonAn <finnson@yeah.net>
2024-01-07 19:33:30 +0800

    完成首页及一部分下单页面的展示

---
+++ admin/src/main/java/com/lab/dev/shawn/api/wx/ticket/controller/WxTicketsController.java
@@ -0,0 +1,47 @@
+package com.lab.dev.shawn.api.wx.ticket.controller;
+
+import com.lab.dev.shawn.api.base.dto.ApiResponseBody;
+import com.lab.dev.shawn.api.util.JwtUtil;
+import com.lab.dev.shawn.api.wx.ticket.service.WxTicketService;
+import com.lab.dev.shawn.api.wx.ticket.vo.WxConcertResponseVO;
+import com.lab.dev.shawn.api.wx.ticket.vo.WxTicketResponseVO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/wx")
+public class WxTicketsController {
+
+    @Autowired
+    private WxTicketService wxTicketService;
+
+    @GetMapping("/concerts")
+    public ResponseEntity<ApiResponseBody> concerts(HttpServletRequest request) {
+
+        String token = request.getHeader("X-Token");
+        Long agentId = JwtUtil.getTokenClaims(token).get("id", Long.class);
+
+        List<WxConcertResponseVO> concerts = wxTicketService.findConcertsByAgentId(agentId);
+        ApiResponseBody body = new ApiResponseBody(concerts);
+        return ResponseEntity.ok(body);
+    }
+
+
+    @GetMapping("/tickets/{concertId}")
+    public ResponseEntity<ApiResponseBody> tickets(@PathVariable("concertId") String concertId, HttpServletRequest request) {
+
+        String token = request.getHeader("X-Token");
+        Long agentId = JwtUtil.getTokenClaims(token).get("id", Long.class);
+
+        WxTicketResponseVO tickets = wxTicketService.findTicketsByAgentIdAndConcertId(agentId, Long.valueOf(concertId));
+        ApiResponseBody body = new ApiResponseBody(tickets);
+        return ResponseEntity.ok(body);
+    }
+}
